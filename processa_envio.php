48e5cc1c6a75488a2e93c17a1acd53b1768f7e8d
Thais Caldas <caldasthais097@gmail.com>
2025-02-18 17:08:31 -0300

    Melhoramdo o feedback visual

--- processa_envio.php
+++ processa_envio.php
@@ -71,3 +71,46 @@
     $mensagem->status['descricao_status'] = "Não foi possível enviar o e-mail. Erro: {$mail->ErrorInfo}";
 
     }
+
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>App Mail Send</title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="py-3 text-center">
+                <img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+                <h2>Send Mail</h2>
+                <p class="lead">Seu app de envio de e-mails particular!</p>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+
+                    <?php if($mensagem->status['codigo_status'] == 1) { ?>
+                        <div class="container">
+                            <h1 class="display-4 text-success">Sucesso</h1>
+                            <p><?= $mensagem->status['descricao_status']?></p> 
+                            <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a> 
+                        </div>
+                    <?php } ?>
+
+                    <?php if($mensagem->status['codigo_status'] == 2) { ?>
+                        <div class="container">
+                        <h1 class="display-4 text-danger">Erro!</h1>
+                        <p><?= $mensagem->status['descricao_status']?></p> 
+                        <a href="index.php" class="btn btn-success btn-lg mt-5 text-white">Voltar</a> 
+                        </div>
+                    <?php } ?>
+
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
